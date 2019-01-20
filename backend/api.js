60b106987c7b8d8a02755b7790b81e46c205fa3c
Darren Lim <darrenlim193@gmail.com>
2019-01-21 10:43:51 +1100

    Initial Commit

---
+++ backend/api.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const axios = require('axios');
+
+const app = new express();
+const port = 5000; //This port can be configurable
+
+//Get navbar dashboard data
+
+//Get Leads Data
+
+//Get Lodgement Data
+
+//Get Approval Data
+
+//Get Settlement Data
+
+//Get ONE user
+
+//Get alll users
