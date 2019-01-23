46e023a75be39b4e18e6dab24404e8be31029fc0
Ben Gelbard <bengelbard@Bens-MacBook-Pro.local>
2019-01-23 11:44:16 +1100

    working on tables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,27 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class leadsPage extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <body>
+<h1 class="header">LEADS</h1>
+<h3 class="CMP">CaptureMortgage+ Leads Board *insert js*</h3>
+<table id="myTable">
+  <tr class="table_header">
+    <th>ID</th>
+    <th>Entered as Lead</th>
+    <th>Customer Name</th>
+    <th>Category</th>
+    <th>Amount</th>
+    <th>Referrer</th>
+    <th>Employee</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>01/01/2019</td>
+    <td>Craig David</td>
+    <td>Home Loan</td>
+    <td>$200,000</td>
+    <td>LP Staff</td>
+    <td>Sarah Smith</td>
+  </tr>
+    <tr>
+    <td>2</td>
+    <td>01/01/2019</td>
+    <td>James Dean</td>
+    <td>Home Loan</td>
+    <td>$40,000</td>
+    <td>LP Staff</td>
+    <td>Louise Lilly</td>
+  </tr>
+    <tr>
+    <td>3</td>
+    <td>02/04/2018</td>
+    <td>Billy Joel</td>
+    <td>Home Loan</td>
+    <td>$475,000</td>
+    <td>LP Staff</td>
+    <td>Katherine Kale</td>
+  </tr>
+</table>
+    
+</body>
     );
   }
 }
 
-export default App;
+export default leadsPage;
