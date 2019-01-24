1a03c613226874fab5ca5a683468921327f85d44
Tiffany F <tbluebox@gmail.com>
2019-01-24 16:44:02 +1100

    Added navbar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -9,22 +9,22 @@ const Navbar = () => {
                         <a href="#left" class="logo">LEFTY</a>
                     </div>
                     <div class="nav-box">
-                    <a href="#dashboard">DASHBOARD</a>
+                        <a href="#dashboard">DASHBOARD</a>
                     <div class="dropdown">
                         <button class="dropbtn">LEADS 
-                        <i class="fa fa-caret-down"></i>
+                            <i class="fa fa-caret-down"></i>
                         </button>
                         <div class="dropdown-content">
-                        <a href="#">New Lead</a>
-                        <a href="#">Employee Leader Board</a>
-                        <a href="#">Referrer Leader Board</a>
+                            <a href="#">New Lead</a>
+                            <a href="#">Employee Leader Board</a>
+                            <a href="#">Referrer Leader Board</a>
                         </div>
                     </div>
                     <a href="#lodgements">LODGEMENTS</a>
                     <a href="#approvals">APPROVALS</a>
                     <a href="#settlements">SETTLEMENTS</a>
                     <a href="#crm">CRM</a>
-                    <a href="#logout">LOGOUT</a> 
+                    <a href="#logout">LOGOUT</a>  
                 </div> 
             </nav>
         </div>
