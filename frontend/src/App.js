15c090cc100f37ad4a0ba4f0ca5145a7086ad175
Tiffany F <tbluebox@gmail.com>
2019-01-30 03:54:28 +1100

    Adding routes and created separated Dashboard page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,42 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.css'
 import Navbar from './Navbar'
-import EmployeeLeaderboard from './tables/EmployeeLeaderboardTable';
-import ReferrerLeaderboard from './tables/ReferrerLeaderboardTable';
+// import Dashboard from './Dashboard'
+import EmployeeLeaderboard from './tables/EmployeeLeaderboardTable'
+import ReferrerLeaderboard from './tables/ReferrerLeaderboardTable'
 import LeadsTable from './tables/LeadsTable'
 import LodgementsTable from './tables/LodgementsTable'
 import ApprovalsTable from './tables/ApprovalsTable'
 import SettlementsTable from './tables/SettlementsTable'
-import CRM from './tables/CrmTable'
-import Form from './Form'
+// import CRM from './tables/CrmTable'
+// import Form from './Form'
+// import { BrowserRouter, Route } from 'react-router-dom'
 
 
 class mainPage extends Component {
   render() {
-    const { pathname } = window.location;
-    const params = pathname.substr(1);
+    // const { pathname } = window.location;
+    // const params = pathname.substr(1);
 
-    if (params === 'leads') {
-      return <LeadsTable />
-    } else if (params === 'leads/new') {
-      return <Form />
-    }  else if (params === 'leads/employeeleaderboard') {
-      return <EmployeeLeaderboard />
-    } else if (params === 'leads/referrerleaderboard') {
-      return <ReferrerLeaderboard />
-    } else if (params === 'lodgements') {
-      return <LodgementsTable />
-    } else if (params === 'approvals') {
-      return <ApprovalsTable />
-    } else if (params === 'settlements') {
-      return <SettlementsTable />
-    } else if (params === 'crm') {
-      return <CRM />
-    } 
+    // if (params === 'dashboard') {
+    //   return <Dashboard />
+    // } else if (params === 'leads') {
+    //   return <LeadsTable />
+    // } else if (params === 'leads/new') {
+    //   return <Form />
+    // }  else if (params === 'leads/employeeleaderboard') {
+    //   return <EmployeeLeaderboard />
+    // } else if (params === 'leads/referrerleaderboard') {
+    //   return <ReferrerLeaderboard />
+    // } else if (params === 'lodgements') {
+    //   return <LodgementsTable />
+    // } else if (params === 'approvals') {
+    //   return <ApprovalsTable />
+    // } else if (params === 'settlements') {
+    //   return <SettlementsTable />
+    // } else if (params === 'crm') {
+    //   return <CRM />
+    // } 
 
     return (
       <div>
@@ -46,10 +50,7 @@ class mainPage extends Component {
           <LodgementsTable />
           <ApprovalsTable />
           <SettlementsTable />
-          <CRM />
         </div>
-
-        <Form/>
 
       </div>
     );
