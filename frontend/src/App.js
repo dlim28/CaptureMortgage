8e8e7ebd7d1ad2e4eac3d3cde7a2057f53fb0e58
Sam <sam.ludwig@gmail.com>
2019-01-29 15:00:06 +1100

    table logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,27 +14,10 @@ import Lightbox from 'lightbox-react';
 import 'lightbox-react/style.css';
 import axios from 'axios'
 
-
+// derp
 
 
 class App extends Component {
-
-//   state = {}
-//   componentDidMount() {
-//     const status0 = axios.get('http://localhost:5000/navbar/getdata/0')
-//     const status1 = axios.get('http://localhost:5000/navbar/getdata/1')
-//     const status2 = axios.get('http://localhost:5000/navbar/getdata/2')
-//     const status3 = axios.get('http://localhost:5000/navbar/getdata/3')
-//     Promise.all([status0, status1, status2, status3])
-//         .then((res) => {
-//             // this.setState({status0: res[0].data.length})
-//             // this.setState({status1: res[1].data.length})
-//             // this.setState({status2: res[2].data.length})
-//             // this.setState({status3: res[3].data.length})
-//             console.log(res)
-//         })
-// }
-
 
   render() {
     return (
